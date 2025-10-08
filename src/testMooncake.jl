7da7b94d2e7fe245e950ac8d88e6dccc07a2916f
Adam T Burke <83083545+adam-t-burke@users.noreply.github.com>
2025-10-07 20:34:12 -0400

    working minimal example

--- src/testMooncake.jl
+++ src/testMooncake.jl
@@ -1,33 +1,94 @@
-function obj(q)   
+#Minimal example demonstrating use of mooncake to minimize objective function. 
+#Redevelopment of theseus will use this approach. 
 
-    xyznew = solve_explicit(q, receiver.Cn, receiver.Cf, receiver.Pn, receiver.XYZf)
-    
-    xyzfull = vcat(xyznew, receiver.XYZf)  
-    
-    lengths = norm.(eachrow(receiver.C * xyzfull))
-    forces = q .* lengths        
+using SparseArrays
+using Mooncake
+using Optim
+import DifferentiationInterface as DI
 
-    loss = lossFunc(xyznew, lengths, forces, receiver, q)
-    
+```
+Explicit solver function
+```
+function solve_explicit(
+    qvec::Vector{Float64},           # Vector of force densities
+    Cn::SparseMatrixCSC{Int64,Int64},# Index matrix of free nodes
+    Cf::SparseMatrixCSC{Int64,Int64},# Index matrix of fixed nodes
+    Pn::Matrix{Float64},             # Matrix of free node loads
+    Nf::Matrix{Float64}              # Matrix of fixed node positions
+)
+    # Scale columns of Cn and Cf by qvec
+    Cnq = Cn .* qvec
+    Cfq = Cf .* qvec
+
+    # Compute result using scaled matrices
+    return (Cn' * Cnq) \ (Pn - Cn' * Cfq * Nf)
+end
+
+function obj(qvec, Cn, Cf, Pn, Nf, target)
+    xyznew = solve_explicit(qvec, Cn, Cf, Pn, Nf)
+    loss = sum((xyznew - target).^2)
     return loss
 end
 
 begin
-    ###Define a simple problem
-    
+    # Problem setup
+    Cn = sparse([1, 2], [1, 1], [1, 1], 2, 1)
+    Cf = sparse([1, 2], [1, 2], [-1, -1], 2, 2)
+    Nf = [0.0 0.0 0.0; 2.0 0.0 0.0]
+    Pn = reshape([0.0, 0.0, -1.0], 1, 3)
 end
 
-function optimize_with_mooncake()
+target_xyz = reshape([0.75, 0.0, -0.75], 1, 3)
 
-    
-            """
-            Mooncake setup
-            """
-            backend = AutoMooncake(; config=nothing)
-            prep = DI.prepare_gradient(obj, backend, parameters)
-            grad = similar(parameters)
-            DI.gradient!(obj, grad, prep, backend, parameters)
+"""
+Mooncake setup
+"""
+backend = DI.AutoMooncake(; config=nothing)
 
-            println(grad)
+qvec = [2.0, 2.0]  # Dense vector of force densities
+prep = DI.prepare_gradient(
+    obj,
+    backend,
+    qvec,
+    DI.Constant(Cn),
+    DI.Constant(Cf),
+    DI.Constant(Pn),
+    DI.Constant(Nf),
+    DI.Constant(target_xyz)
+)
+grad = similar(qvec)
 
-end
+DI.gradient!(
+    obj,
+    grad,
+    prep,
+    backend,
+    qvec,
+    DI.Constant(Cn),
+    DI.Constant(Cf),
+    DI.Constant(Pn),
+    DI.Constant(Nf),
+    DI.Constant(target_xyz)
+)
+
+result = optimize(
+    q -> obj(q, Cn, Cf, Pn, Nf, target_xyz),  # objective as closure
+    [2.0, 2.0],                              # initial guess
+    BFGS()
+)
+
+println("Optimal qvec: ", Optim.minimizer(result))
+println("Minimum loss: ", Optim.minimum(result))
+
+# Final solve with optimal qvec
+final_xyz = solve_explicit(
+    Optim.minimizer(result),
+    Cn,
+    Cf,
+    Pn,
+    Nf
+)
+println("Final node location: ", final_xyz)
+
+
+
